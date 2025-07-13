09615ecd85e0b710548d5c896c9a9ad98c84d72e
Mohammad Sajeed <mohammadsajeed7862@gmail.com>
2025-07-13 15:40:55 +0530

    Added LeetCode: Remove Element – Jul 13, 2025

---
+++ 2025/July/13-remove-element.cpp
@@ -0,0 +1,17 @@
+// LeetCode Problem: Remove Element
+// Solved on: July 13, 2025
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;        
+    }
+};
